b998d0d1009194f20823228944fc1603ed5099b1
Riya Mangal <riyamangaljaipur#gmail.com>
2026-04-17 18:12:35 +0530

    updated: add root redirect to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
@@ -9,6 +9,7 @@ function App() {
     <div className="bg-red-500 min-h-screen">
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />}></Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
